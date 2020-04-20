4f423aaf7864ffb818a67c1c168bc2756d15e096
Mayank Garg <mayank.captain00@gmail.com>
2020-04-21 02:14:59 +0530

    Add files via upload

---
+++ adopt-me/src/App.js
@@ -0,0 +1,39 @@
+const Pet = ({name, animal, breed}) => {
+    return React.createElement(
+        "div", {},
+        [
+            React.createElement("h1", {}, name),
+            React.createElement("h2", {}, animal),
+            React.createElement("h2", {}, breed)
+        ]
+    );
+};
+
+
+const App = () => {
+    return React.createElement(
+        "div", {
+            id: "something-important"
+        },
+        [
+            React.createElement("h1", {}, "Adopt Me!"),
+            React.createElement(Pet, {
+                name: "Luna",
+                animal: "Dog",
+                breed: "Retriever"
+            }),
+            React.createElement(Pet, {
+                name: "Thomas",
+                animal: "Cat",
+                breed: "Mixed"
+            }),
+            React.createElement(Pet, {
+                name: "Mithu",
+                animal: "Bird",
+                breed: "Parrot"
+            }),
+        ]
+    );
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"))
