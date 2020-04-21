7b094e0f138dbc9cd7008beb2ccae65fbaa43eae
Mayank <mayank.captain00@gmail.com>
2020-04-21 20:20:14 +0530

    Added ESLint, Prettier

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,14 +1,14 @@
-const Pet = ({name, animal, breed}) => {
-    return React.createElement(
-        "div", {},
-        [
-            React.createElement("h1", {}, name),
-            React.createElement("h2", {}, animal),
-            React.createElement("h2", {}, breed)
-        ]
-    );
+const Pet = ({
+    name,
+    animal,
+    breed
+}) => {
+    return React.createElement("div", {}, [
+        React.createElement("h1", {}, name),
+        React.createElement("h2", {}, animal),
+        React.createElement("h2", {}, breed)
+    ]);
 };
-
 
 const App = () => {
     return React.createElement(
@@ -31,9 +31,9 @@ const App = () => {
                 name: "Mithu",
                 animal: "Bird",
                 breed: "Parrot"
-            }),
+            })
         ]
     );
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"))
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
