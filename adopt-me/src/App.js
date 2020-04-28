74a12ddd4a2f124b016c6d76af2fba7cbe3baad7
Mayank <mayank.captain00@gmail.com>
2020-04-29 02:21:08 +0530

    async calls to petfinder API

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,33 +1,15 @@
-import React from 'react';
-import {
-    render
-} from 'react-dom';
-import Pet from './Pet';
+import React from "react";
+import { render } from "react-dom";
+
+import SearchParams from "./SearchParams";
 
 const App = () => {
-    return React.createElement(
-        "div", {
-            id: "something-important"
-        },
-        [
-            React.createElement("h1", {}, "Adopt Me!"),
-            React.createElement(Pet, {
-                name: "Luna",
-                animal: "Dog",
-                breed: "Retriever"
-            }),
-            React.createElement(Pet, {
-                name: "Thomas",
-                animal: "Cat",
-                breed: "Mixed"
-            }),
-            React.createElement(Pet, {
-                name: "Mithu",
-                animal: "Bird",
-                breed: "Parrot"
-            })
-        ]
-    );
+	return (
+		<div>
+			<h1 id="something-important">Adopt Me!</h1>
+			<SearchParams />
+		</div>
+	);
 };
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
---
+++ adopt-me/src/SearchParams.js
@@ -0,0 +1,97 @@
+import React, { useState, useEffect } from "react";
+import pet, { ANIMALS } from "@frontendmasters/pet";
+import Results from './Results';
+import useDropdown from "./useDropdown";
+
+const SearchParams = () => {
+	// Here we are destructuring it.
+	// useState returns array two things: State & Updater functiom.
+	const [location, setLocation] = useState("Seattle, WA");
+	const [breeds, setBreeds] = useState([]);
+	const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
+	const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+	const [pets, setPets] = useState([]);
+
+
+	async function requestPets() {
+		const { animals } = await pet.animals({
+		  location,
+		  breed,
+		  type: animal
+		});
+	
+		console.log("animals", animals);
+	
+		setPets(animals || []);
+	  }
+
+	useEffect(() => {
+		setBreeds([]);
+		setBreed("");
+
+		pet.breeds(animal).then(({ breeds: apiBreeds }) => {
+			const breedString = apiBreeds.map(({ name }) => name);
+			setBreeds(breedString);
+		}, console.error);
+	}, [animal, setBreed, setBreeds]);
+	// console.log(pet.breeds("dog"))
+
+	return (
+		<div className="search-params">
+			<form onSubmit={(e) => {
+				e.preventDefault();
+				requestPets();
+			}}
+			>
+				<label htmlFor="location">
+					Location
+					<input
+						id="location"
+						value={location}
+						placeholder="location"
+						onChange={event => setLocation(event.target.value)}
+					/>
+				</label>
+				{/* <label htmlFor="animal">
+					Animal
+					<select
+						id="amimal"
+						value={animal}
+						onChange={event => setAnimal(event.target.value)}
+						onBlur={e => setAnimal(e.target.value)}
+					>
+						<option>All</option>
+						{ANIMALS.map(animal => (
+							<option key={animal} value={animal}>
+								{animal}
+							</option>
+						))}
+					</select>
+				</label>
+				<label htmlFor="breed">
+					Breed
+					<select
+						id="breed"
+						value={breed}
+						onChange={e => setBreed(e.target.value)}
+						onBlur={e => setBreed(e.target.value)}
+						disabled={breeds.length === 0}
+					>
+						<option>All</option>
+						{breeds.map(breedString => (
+							<option key={breedString} value={breedString}>
+								{breedString}
+							</option>
+						))}
+					</select>
+				</label> */}
+				<AnimalDropdown />
+				<BreedDropdown />
+				<button>Submit</button>
+				<Results pets={pets} />
+			</form>
+		</div>
+	);
+};
+
+export default SearchParams;
